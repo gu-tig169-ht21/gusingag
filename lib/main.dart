9d5ad28350da10f0038e75f09a6384190e3af94f
ingheden <ingheden.agaton@gmail.com>
2021-12-02 19:47:40 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/Todosprovider.dart';
 import 'package:provider/provider.dart';
+import 'package:my_first_app/home.dart';
+<<<<<<< Updated upstream
 
 
+=======
+import 'package:my_first_app/home.dart';
+>>>>>>> Stashed changes
 
 void main() async {
   runApp(const MyApp());
@@ -11,22 +16,24 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     var ip = TodosProvider();
     ip.getName();
     return ChangeNotifierProvider(
-      create: (context) => ip,
-      child: MaterialApp(
-        title: 'Att göra idag',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Home()));
+        create: (context) => ip,
+        child: MaterialApp(
+            title: 'Att göra idag',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.deepPurple,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: Home()));
   }
 }
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
